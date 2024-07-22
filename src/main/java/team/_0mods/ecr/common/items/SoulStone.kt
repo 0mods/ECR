46ff63f503125e230e0af853bdfa5fc32450fe0c
algorithmlx <izhukov2007@gmail.com>
2024-07-22 15:13:10 +0700

    fixes

--- src/main/java/team/_0mods/ecr/common/items/SoulStone.kt
+++ src/main/java/team/_0mods/ecr/common/items/SoulStone.kt
@@ -1,28 +1,71 @@
 package team._0mods.ecr.common.items
 
+import net.minecraft.client.multiplayer.PlayerInfo
+import net.minecraft.network.chat.Component
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResultHolder
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
+import team._0mods.ecr.ModId
 import team._0mods.ecr.common.init.registry.ECTabs
 import java.util.UUID
 
 class SoulStone: Item(Properties().tab(ECTabs.tabItems)) {
-    init {
+    companion object {
+
+
+        @JvmStatic
         fun isBounded(stack: ItemStack): Boolean {
+            if (stack.item !is SoulStone) throw UnsupportedOperationException()
             val tag = stack.orCreateTag
             return tag.contains("SoulStoneOwner")
         }
 
+        @JvmStatic
         fun getBoundedTo(stack: ItemStack): UUID? {
+            if (stack.item !is SoulStone) throw UnsupportedOperationException()
             val tag = stack.orCreateTag
             return if (isBounded(stack)) tag.getUUID("SoulStoneOwner") else null
         }
 
+        @JvmStatic
         fun setBoundedTo(stack: ItemStack, player: Player) {
+            if (stack.item !is SoulStone) throw UnsupportedOperationException()
             val tag = stack.orCreateTag
             if (!isBounded(stack)) {
                 tag.putUUID("SoulStoneOwner", player.uuid)
             }
         }
+
+        @JvmStatic
+        fun getCapacity(stack: ItemStack): Int {
+            if (stack.item !is SoulStone) throw UnsupportedOperationException()
+            val tag = stack.orCreateTag
+            if (!tag.contains("SoulStoneCapacity")) {
+                tag.putInt("SoulStoneCapacity", 0)
+            }
+
+            if (!isBounded(stack)) {
+                tag.putInt("SoulStoneCapacity", 0)
+            }
+
+            return tag.getInt("SoulStoneCapacity")
+        }
+    }
+
+    override fun use(level: Level, player: Player, usedHand: InteractionHand): InteractionResultHolder<ItemStack> {
+        val stack = player.getItemInHand(usedHand)
+
+        if (!player.isShiftKeyDown) {
+            if (!isBounded(stack)) {
+                setBoundedTo(stack, player)
+                player.displayClientMessage(Component.translatable("info.$ModId.soul_stone.bounded", player.name), true)
+                return InteractionResultHolder.success(stack)
+            }
+        }
+
+        return super.use(level, player, usedHand)
     }
 }
