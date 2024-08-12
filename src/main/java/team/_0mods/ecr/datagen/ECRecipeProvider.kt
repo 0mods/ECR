1c5766dff0d2d1266ff2f72be113cb1779cb1171
algorithmlx <izhukov2007@gmail.com>
2024-08-12 22:30:53 +0700

    recipe updates

--- src/main/java/team/_0mods/ecr/datagen/ECRecipeProvider.kt
+++ src/main/java/team/_0mods/ecr/datagen/ECRecipeProvider.kt
@@ -11,6 +11,7 @@ class ECRecipeProvider(generator: DataGenerator) : RecipeProvider(generator) {
     override fun buildCraftingRecipes(c: Consumer<FinishedRecipe>) {
         MithrilineFurnaceRecipeBuilder.make(Items.GOLD_INGOT).requires(Items.IRON_INGOT, 8).espe(64).save(c)
         MithrilineFurnaceRecipeBuilder.make(Items.IRON_INGOT, 8).requires(Items.GOLD_INGOT).espe(64).save(c)
+
         MithrilineFurnaceRecipeBuilder.make(Items.EMERALD).requires(Items.DIAMOND, 2).espe(512).save(c)
         MithrilineFurnaceRecipeBuilder.make(Items.DIAMOND, 2).requires(Items.EMERALD).espe(512).save(c)
 
@@ -79,5 +80,35 @@ class ECRecipeProvider(generator: DataGenerator) : RecipeProvider(generator) {
         MithrilineFurnaceRecipeBuilder.make(Items.SPRUCE_LEAVES).requires(Items.JUNGLE_LEAVES).espe(1).save(c)
         MithrilineFurnaceRecipeBuilder.make(Items.DARK_OAK_LEAVES).requires(Items.SPRUCE_LEAVES).espe(1).save(c)
         MithrilineFurnaceRecipeBuilder.make(Items.MANGROVE_LEAVES).requires(Items.DARK_OAK_LEAVES).espe(1).save(c)
+
+        MithrilineFurnaceRecipeBuilder.make(Items.PUMPKIN).requires(Items.MELON).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.MELON).requires(Items.PUMPKIN).espe(1).save(c)
+
+        MithrilineFurnaceRecipeBuilder.make(Items.DANDELION).requires(Items.CORNFLOWER).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.POPPY).requires(Items.DANDELION).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.BLUE_ORCHID).requires(Items.POPPY).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.ALLIUM).requires(Items.BLUE_ORCHID).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.AZURE_BLUET).requires(Items.ALLIUM).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.RED_TULIP).requires(Items.AZURE_BLUET).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.ORANGE_TULIP).requires(Items.RED_TULIP).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.WHITE_TULIP).requires(Items.ORANGE_TULIP).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.PINK_TULIP).requires(Items.WHITE_TULIP).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.OXEYE_DAISY).requires(Items.PINK_TULIP).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.CORNFLOWER).requires(Items.OXEYE_DAISY).espe(1).save(c)
+
+        MithrilineFurnaceRecipeBuilder.make(Items.RED_MUSHROOM).requires(Items.BROWN_MUSHROOM).espe(1).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.BROWN_MUSHROOM).requires(Items.RED_MUSHROOM).espe(1).save(c)
+
+        MithrilineFurnaceRecipeBuilder.make(Items.ENDER_CHEST, 3).requires(Items.BLAZE_ROD, 2).espe(128).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.BLAZE_ROD, 2).requires(Items.ENDER_CHEST, 3).espe(128).save(c)
+
+        MithrilineFurnaceRecipeBuilder.make(Items.REDSTONE, 64).requires(Items.GHAST_TEAR).espe(1024).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.GHAST_TEAR).requires(Items.REDSTONE, 64).espe(1024).save(c)
+
+        MithrilineFurnaceRecipeBuilder.make(Items.CLAY, 12).requires(Items.GUNPOWDER).espe(32).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.GUNPOWDER).requires(Items.CLAY, 12).espe(32).save(c)
+
+        MithrilineFurnaceRecipeBuilder.make(Items.SUGAR, 3).requires(Items.SLIME_BALL, 4).espe(16).save(c)
+        MithrilineFurnaceRecipeBuilder.make(Items.SLIME_BALL, 4).requires(Items.SUGAR, 3).espe(16).save(c)
     }
 }
