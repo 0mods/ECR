f7baf228cc270612110f5d059db0855e6d280301
algorithmlx <izhukov2007@gmail.com>
2024-08-20 02:49:11 +0700

    .

--- src/main/java/team/_0mods/ecr/common/init/events/ModCommonEvents.kt
+++ src/main/java/team/_0mods/ecr/common/init/events/ModCommonEvents.kt
@@ -1,4 +1,4 @@
-@file:Mod.EventBusSubscriber(modid = ModId, bus = Mod.EventBusSubscriber.Bus.MOD)
+@file:Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 
 package team._0mods.ecr.common.init.events
 
@@ -7,7 +7,6 @@ import kotlinx.serialization.json.Json
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
-import team._0mods.ecr.ModId
 import team._0mods.ecr.api.utils.loadConfig
 import team._0mods.ecr.common.init.config.ECCommonConfig
 import team._0mods.ecr.common.init.registry.ECRecipes
