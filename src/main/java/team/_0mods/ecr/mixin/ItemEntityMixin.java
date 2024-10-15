b737013fffff65e23542130a0da5155063c3d8a7
algorithmlx <izhukov2007@gmail.com>
2024-10-15 12:02:19 +0700

    Texture update & block checking

--- src/main/java/team/_0mods/ecr/mixin/ItemEntityMixin.java
+++ src/main/java/team/_0mods/ecr/mixin/ItemEntityMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -36,25 +37,26 @@ public abstract class ItemEntityMixin extends Entity {
     public void tick(CallbackInfo ci) {
         if (this.getItem().is(Items.EMERALD)) {
             var bl = new BlockPos(this.position().x(), this.position().y() - 1, this.position().z());
-            if (ECRMultiblocks.INSTANCE.getSoulStone().get().isValid(this.level, bl)) {
-                var rand = new Random();
+            if (level.getBlockState(bl).is(Blocks.EMERALD_BLOCK)) {
+                if (ECRMultiblocks.INSTANCE.getSoulStone().get().isValid(this.level, bl)) {
+                    var rand = new Random();
 
-                var pos = position();
-                level.addParticle(new ECParticleOptions(Color.GREEN, 0.5f, 40, 0.05f, false, false),
-                        pos.x, pos.y + 0.5, pos.z, 1.0, 1.0, 1.0);
+                    var pos = position();
+                    level.addParticle(new ECParticleOptions(Color.GREEN, 0.5f, 40, 0.05f, false, false),
+                            pos.x, pos.y + 0.5, pos.z, 1.0, 1.0, 1.0);
 
+                    if (ecr$tickCount++ >= 40) {
+                        ecr$tickCount = 0;
+                        this.getItem().shrink(1);
 
-                if (ecr$tickCount++ >= 40) {
-                    ecr$tickCount = 0;
-                    this.getItem().shrink(1);
-
-                    if (rand.nextInt(10) > 6) {
-                        var item = new ItemEntity(level, this.position().x(), this.position().y(), this.position().z(), new ItemStack(ECRegistry.INSTANCE.getSoulStone().get()));
-                        item.setNoPickUpDelay();
-                        level.addFreshEntity(item);
+                        if (rand.nextInt(10) > 6) {
+                            var item = new ItemEntity(level, this.position().x(), this.position().y(), this.position().z(), new ItemStack(ECRegistry.INSTANCE.getSoulStone().get()));
+                            item.setNoPickUpDelay();
+                            level.addFreshEntity(item);
+                        }
                     }
-                }
-            } else ecr$tickCount = 0;
+                } else ecr$tickCount = 0;
+            }
         }
     }
 }
