8cb017b2d4051264247d4920ddf260d539d45e89
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2025-01-04 17:07:01 +0700

    screen update

--- src/main/java/team/_0mods/ecr/client/screen/ECBookScreen.kt
+++ src/main/java/team/_0mods/ecr/client/screen/ECBookScreen.kt
@@ -1,29 +1,26 @@
 package team._0mods.ecr.client.screen
 
 import de.fabmax.kool.modules.ui2.*
-import de.fabmax.kool.pipeline.SamplerSettings
-import de.fabmax.kool.pipeline.Texture
-import de.fabmax.kool.pipeline.Texture2d
-import de.fabmax.kool.pipeline.TextureProps
-import de.fabmax.kool.pipeline.backend.gl.GlTexture
-import de.fabmax.kool.pipeline.backend.gl.LoadedTextureGl
+import de.fabmax.kool.pipeline.FullscreenShaderUtil.generateFullscreenQuad
 import de.fabmax.kool.pipeline.shading.BlurShader
 import de.fabmax.kool.pipeline.shading.BlurShaderConfig
 import de.fabmax.kool.scene.Scene
-import de.fabmax.kool.scene.addColorMesh
+import de.fabmax.kool.scene.addTextureMesh
+import de.fabmax.kool.util.Color
+import net.minecraft.client.Minecraft
 import net.minecraft.resources.ResourceLocation
+import ru.hollowhorizon.hc.client.imgui.glTexture
 import ru.hollowhorizon.hc.client.kool.KoolScreen
-import ru.hollowhorizon.hc.client.kool.MCGlApi
-import ru.hollowhorizon.hc.client.render.RenderLoader
 import ru.hollowhorizon.hc.client.utils.toTexture
 import team._0mods.ecr.api.item.Research
 import team._0mods.ecr.api.utils.ecRL
 import team._0mods.ecr.common.data.ResearchBookData
 
 class ECBookScreen(private val type: List<Research>, val data: ResearchBookData): KoolScreen({
-    if (data.selectedResearch == null) {
+    /*if (data.selectedResearch == null) {
         renderDefaultBG("textures/gui/book/book.png".ecRL)
-    }
+    }*/
+    renderDefaultBG("textures/gui/book/book.png".ecRL)
 }) {
     override fun isPauseScreen(): Boolean = false
 }
@@ -33,27 +30,20 @@ fun Scene.renderDefaultBG(
     needBlur: Boolean = true,
     blurRadius: Int = 8
 ) {
-    setupUiScene(Scene.DEFAULT_CLEAR_COLOR)
+    setupUiScene(Color(0f, 0f, 0f, 0f))
 
-    addColorMesh {
-        if (needBlur) shader = BlurShader(BlurShaderConfig().apply { kernel = BlurShader.blurKernel(blurRadius) })
+    if (needBlur) {
+        addTextureMesh {
+            generateFullscreenQuad()
+            shader = BlurShader(BlurShaderConfig().apply { kernel = BlurShader.blurKernel(blurRadius) }).apply {
+                blurInput = glTexture(Minecraft.getInstance().mainRenderTarget.colorTextureId)
+            }
+        }
     }
 
     addPanelSurface {
-        Image(minecraftTexture(texture)) {
+        Image(glTexture(texture.toTexture().id)) {
             modifier.size(512.dp, 256.dp)
         }
     }
-}
-
-fun UiScope.minecraftTexture(texture: ResourceLocation): Texture2d = Texture2d(TextureProps(generateMipMaps = false, defaultSamplerSettings = SamplerSettings().clamped().nearest())).apply {
-    gpuTexture = LoadedTextureGl(
-        MCGlApi.TEXTURE_2D,
-        GlTexture(texture.toTexture().id),
-        MCGlApi.backend,
-        this,
-        0
-    )
-
-    loadingState = Texture.LoadingState.LOADED
 }
--- src/main/java/team/_0mods/ecr/common/data/ResearchBookData.kt
+++ src/main/java/team/_0mods/ecr/common/data/ResearchBookData.kt
@@ -1,6 +1,7 @@
 package team._0mods.ecr.common.data
 
+import kotlinx.serialization.Serializable
 import team._0mods.ecr.api.item.Research
 
-class ResearchBookData(var selectedResearch: Research? = null) {
-}
+@Serializable
+class ResearchBookData(var selectedResearch: Research? = null)
