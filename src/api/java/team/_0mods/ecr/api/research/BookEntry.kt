b20eac61b0e48b2b23e81bfbc738e93da55f2dfc
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2025-03-19 16:24:45 +0700

    ...

--- src/api/java/team/_0mods/ecr/api/research/BookEntry.kt
+++ src/api/java/team/_0mods/ecr/api/research/BookEntry.kt
@@ -1,5 +1,33 @@
 package team._0mods.ecr.api.research
 
+import de.fabmax.kool.scene.Scene
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
+
 interface BookEntry {
     val entryDatas: List<BookEntryData>
+}
+
+interface BookEntryData {
+    val content: BookContent
+
+    val dependency: Dependency
+
+    data class Dependency(val dependency: BookEntryData, val align: DependencyAlign) {
+        enum class DependencyAlign {
+            AFTER, BEFORE
+        }
+    }
+}
+
+interface BookContent {
+    val render: Scene.() -> Unit get() = {}
+
+    val serializer: BookContentSerializer<*>
+}
+
+interface BookContentSerializer<T: BookContent> {
+    fun toJson(content: T): JsonObject
+
+    fun fromJson(jsonObject: JsonObject): T
 }
--- src/api/java/team/_0mods/ecr/api/research/BookEntryData.kt
+++
@@ -1,7 +0,0 @@
-package team._0mods.ecr.api.research
-
-import de.fabmax.kool.scene.Scene
-
-interface BookEntryData {
-    val render: Scene.() -> Unit
-}
