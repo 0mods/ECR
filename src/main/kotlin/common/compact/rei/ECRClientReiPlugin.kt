091e9f223ab0e6eb1087285732d58135f40b1943
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2025-04-29 23:48:12 +0700

    rei category

--- src/main/kotlin/common/compact/rei/ECRClientReiPlugin.kt
+++ src/main/kotlin/common/compact/rei/ECRClientReiPlugin.kt
@@ -1,7 +1,18 @@
 package team._0mods.ecr.common.compact.rei
 
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin
-import me.shedaniel.rei.api.common.plugins.REIServerPlugin
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry
+import team._0mods.ecr.common.compact.rei.category.EnvoyerDisplayCategory
+import team._0mods.ecr.common.compact.rei.display.EnvoyerDisplay
+import team._0mods.ecr.common.recipes.XLikeRecipe
 
 class ECRClientReiPlugin: REIClientPlugin {
+    override fun registerCategories(registry: CategoryRegistry?) {
+        registry?.add(EnvoyerDisplayCategory)
+    }
+
+    override fun registerDisplays(registry: DisplayRegistry?) {
+        registry?.registerFiller(XLikeRecipe.Envoyer::class.java) { EnvoyerDisplay(it) }
+    }
 }
